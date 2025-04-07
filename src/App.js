6ead21acc8048dbef22d42ee7f7f037d77de0ab2
Smoonun <brahitonkevin@gmail.com>
2025-04-07 12:48:46 -0600

    moficicacion de texto con hipervinculo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Viva la React!
         </a>
       </header>
     </div>
