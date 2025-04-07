3af35f66ac479dafa29cf06895ee3b4489f0c809
Smoonun <brahitonkevin@gmail.com>
2025-04-07 16:23:23 -0600

    Interfaz Login

---
+++ src/Ilogin.jsx
@@ -0,0 +1,19 @@
+export default function Ilogin(){
+  return(<>
+        <header>RHORF Soluciones Industriales</header>
+        <nav>Aqui va nuestro logo y la barra de navegacion </nav>
+        <seccion>
+          <div>Usuario:</div>
+          <h1>Input box</h1>
+          <div>
+            Password:
+          <h1>Inout box </h1>
+          </div>
+          <button>Iniciar Sesion</button>
+          <button>Olvide mi contraseña</button>
+
+        </seccion>
+        
+    </>
+  )
+}
