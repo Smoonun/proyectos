f7834eee849bc661c740f558082d379ec1938ca6
Smoonun <brahitonkevin@gmail.com>
2025-04-07 18:15:35 -0600

    login index instagram

--- src/Ilogin.jsx
+++ src/Ilogin.jsx
@@ -1,18 +1,28 @@
+import './Ilogin.css';
 export default function Ilogin(){
   return(<>
-        <header>RHORF Soluciones Industriales</header>
-        <nav>Aqui va nuestro logo y la barra de navegacion </nav>
-        <section>
-          <div>Usuario:</div>
-          <h1>Input box</h1>
-          <div>
-            Password:
-          <h1>Inout box </h1>
-          </div>
-          <button>Iniciar Sesion</button>
-          <button>Olvide mi contraseña</button>
+        <form class ="container">
+          <h1 class="login-title">Login</h1>
+          <section class="input-box">
+            <input type="text" name="username" placeholder="Username"/>
+            <i class='bx bxs=lock-alt'></i>
+          </section>
+          <section class="input-box">
+            <input type="password" name="password" placeholder="Password"/>
+            <i class='bx bxs=lock-alt'></i>
+          </section>
+          <section class="remember-forgot-box">
+            <div class="remember-me">
+              <input type="checkbox" name="remember-me" id="remember-me"/>
+              <label for="remember-me">Remember me</label>
+              <h5>Remember me </h5>
+            </div>
+          <a class="forgot-password" href="#">Forgot password?</a>
+          </section>
+          <button class="login-btn" type="submit">Login</button>
+          <h5 class="signup-link">Don't have an account? <a href="#">Sign up</a></h5>
 
-        </section>
+          </form> 
         
     </>
   )
