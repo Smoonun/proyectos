d5b32ff3ba8f25494aca81d9c57bb567a6724233
Smoonun <brahitonkevin@gmail.com>
2025-04-25 20:56:26 -0600

    creacion de la api

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,9 +5,25 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const db = mysql.createConnection({
+    host: 'localhost',
+     user:'root',
+     password:'',
+     database:'datos_pruebas'
+})
+
 app.get('/', (re, res) => {
         return res.json("From Backend side ");
 })
+
+app.get('/datos', (req, res) => {
+    const sql = "SELECT * FROM datos";
+    db .query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+}
+)
 
 app.listen(8081, ()=>{
     console.log("listening on port 8081");
