56bd1fb712f4f44b24df22cd896b02b89fe927ce
Smoonun <brahitonkevin@gmail.com>
2025-05-03 01:53:33 -0600

    Correcion de paleta de colores

--- Aplication/mi-nueva-app/src/component/Jefe.jsx
+++ Aplication/mi-nueva-app/src/component/Jefe.jsx
@@ -1,12 +1,58 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Pie from './Pie'
+import Gafete from "./Gafete"
+import './jefe.css'
+import SenialAndon from "./SenialAndon"
 export default function Jefe  ()  {
   const navigate =useNavigate();
   return (
-    <div>Jefe
-      <button onClick= {()=>{navigate('/')}}>Login </button>
+      <> 
+        <div class="parent_jefe">
+          <div class="div1"><Gafete name="Erick A. Hernandez Aguirre" positon="Jefe de Produccion" employeeId="Bob Elegante"/> </div>
+          <div class="div2">
+            <table>
+              <thead>
+                <tr>
+                  <th>
+                    # Empleado
+                  </th>
+                  <th>
+                    Estatus
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> 1</td>
+                  <td><SenialAndon/></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="div3">
+          <table>
+              <thead>
+                <tr>
+                  <th>
+                    Actividad a realizar
+                  </th>
+                  <th>
+                    Estatus
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> Recorrido general por el area</td>
+                  <td><input type="checkbox"></input></td>
+                </tr>
+              </tbody>
+            </table>
+          <button onClick= {()=>{navigate('/')}}>Login </button>
+          </div>
+</div>
       <Pie/>
-    </div>
+    </>
   )
 }
---
+++ Aplication/mi-nueva-app/src/component/SenialAndon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SenialAndon() {
+  return (
+    <div classname="container">
+        <div className="semaforo">
+            <span className="luces-circulo"></span>
+            <span className="luces-circulo"></span>
+            <span className="luces-circulo"></span>
+        </div>
+    </div>
+  )
+}
