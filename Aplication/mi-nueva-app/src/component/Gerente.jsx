7878a87f2e541e1e41780319197370af7033705e
Smoonun <brahitonkevin@gmail.com>
2025-05-05 02:17:09 -0600

    Interfaz gerente(estetica)

--- Aplication/mi-nueva-app/src/component/Gerente.jsx
+++ Aplication/mi-nueva-app/src/component/Gerente.jsx
@@ -1,12 +1,98 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import './gerente.css'
 import Pie from './Pie'
+import Gafete from './Gafete'
+import SenialAndon from './SenialAndon'
 export default function Gerente() {
   const navigate = useNavigate();
   return (
-    <div>Gerente
-      <button onClick= {()=>{navigate('/')}}>Login </button>
+    <> 
+      <div className="parentg">
+        <div className="div1g">
+          <span className="spangafete">
+          <Gafete/>
+          <button onClick= {()=>{navigate('/')}}>Login </button>
+          </span>
+        </div>
+        <div class="div2g">
+          <span className="gerenteNav">
+            <button className="btngerente">Jefe</button>
+            <button className="btngerente">Inventario</button>
+            <button className="btngerente">Almacen</button>
+          </span>
+        </div>
+        <div class="div3g">
+          <table>
+            <thead>
+              <tr>
+                <th># Pedido</th>
+                <th>Estatus</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>En proceso</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="div4g">
+          <table>
+            <thead>
+              Sin existencias
+              <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Madera comprimida</td>
+                <td>Color turquesa</td>
+              </tr>
+              <tr>
+                <td>Madera comprimida</td>
+                <td>Color caoba</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="div5g">
+          <table>
+            <thead>
+              <tr>
+                <th>#Pedido</th>
+                <th>Estacion</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Corte</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="div6g">
+          <table>
+            <thead>
+              <tr>
+                <th># Operador</th>
+                <th>Estatus</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><SenialAndon/></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
       <Pie/>
-    </div>
+    </>
   )
 }
